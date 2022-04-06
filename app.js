12521cfb1db52ea65b77034484bf7d24a228dcc3
Bilal <99763904+bial9@users.noreply.github.com>
2022-04-07 01:12:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+// var arr=[
+//     [    ],
+//     [    ],
+//     [    ]
+// ];
+
+// var arr=[
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ];
+// document.write(arr[0]+"<br>")
+// document.write(arr[1]+"<br>")
+// document.write(arr[2]+"<br>")
+
+// for (var i = 0; i <= 10 ; i++) {
+//     document.write(i+"<br>")
+// }
+
+// var multiply=+prompt("Table number")
+// var length=+prompt("length")
+
+// for (var i=1 ;  i <= length ; i++) {
+//     document.write(multiply+"x "+i+"="+multiply*i+"<br>")
+// }
+
+
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"];
+
+
+
+// for (var i = 0; i < fruits.length; i++) {
+    
+    
+//     document.write("Element at index "+i+" is "+fruits[i]+"<br>"+"<br>")
+    
+// }
+    
+// for(var i=1; i <= 15; i++){
+//     document.write(i+",")
+// }
+
+// for(var i=10; i >= 1; i--){
+//     document.write(i+",")
+// }
+
+// for(var i=0; i <=20; i+=2){
+//     document.write(i+",")
+// }
+
+// for(var i=1; i <=19; i+=2){
+//     document.write(i+",")
+// }
+
+// for(var i=2; i <=20; i+=2){
+//     document.write(i+"k,")
+// }
+
+// for(var i=1; i <= 20; i++){
+//     document.write(5*i+",")
+// }
+
+// const cars = new Array("Saab", "Volvo", "BMW");
+// const person = {firstName:"John", lastName:"Doe", age:46};
+
+// var education = ["ssc","hsc","bcs","bs","bcom"];
+// for(var i=0; i < education.length; i++){
+//     document.write(i+")"+education[i]+"<br>");
+// }
+
+// var devices = ["keyboard","mouse","printer","monitor"]
+// document.write(devices[0]+"<br>")
+// document.write(devices[1]+"<br>")
+// document.write(devices[2]+"<br>")
+// document.write(devices[3]+"<br>")
+
+// var arr = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+// document.write("<select>");
+// document.write("<option>"  + arr[0] +"</option>");
+// document.write("<option>"  + arr[1] +"</option>");
+// document.write("<option>"  + arr[2] +"</option>");
+// document.write("</select>");
